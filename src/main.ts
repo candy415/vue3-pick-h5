1e432c66f02a51b79eb39fdc384fe4edc2918e62
flying-hu <huwanfei125@163.com>
2023-08-29 14:40:41 +0800

    feat✨: 自动引入第三方组件库以及自定义组件

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: huwanfei
  * @Date: 2023-08-04 10:59:36
- * @LastEditTime: 2023-08-04 15:28:02
+ * @LastEditTime: 2023-08-29 14:34:23
  * @LastEditors: huwanfei
  * @Description:
  * @FilePath: /vue3-pick-h5/src/main.ts
@@ -14,12 +14,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import vant from './common/vantui'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(vant)
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: huwanfei
  * @Date: 2023-08-04 10:59:36
- * @LastEditTime: 2023-08-04 15:06:56
+ * @LastEditTime: 2023-08-29 14:30:41
  * @LastEditors: huwanfei
  * @Description:
  * @FilePath: /vue3-pick-h5/vite.config.ts
@@ -13,18 +13,39 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
-import AutoImport from "unplugin-auto-import/vite"
+import AutoImport from 'unplugin-auto-import/vite'
+import {
+  createStyleImportPlugin,
+  VantResolve
+} from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    // vant按需引入样式
+    createStyleImportPlugin({
+      resolves: [
+        VantResolve()
+      ],
+      // 自定义规则
+      libs: [
+        {
+          libraryName: 'vant',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `vant/es/${name}/style`
+          }
+        }
+      ]
+    }),
     Components({
+      dirs: ['src/components'], // 配置需要默认导入的自定义组件文件夹，该文件夹下的所有组件都会自动 import, 默认配置src/components
       resolvers: [VantResolver()]
     }),
-    AutoImport ({
-      imports: ["vue", "vue-router","pinia"], //自动引入vue的ref、toRefs、onmounted等，无需在页面中再次引入
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia'], //自动引入vue的ref、toRefs、onmounted等，无需在页面中再次引入
       dts: 'src/types/auto-import.d.ts', // 根据引入来源自动生成的类型声明文件路径
       eslintrc: {
         enabled: true // 使用 eslint 配置
